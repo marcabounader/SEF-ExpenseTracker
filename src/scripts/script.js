f35a4001bee4238e61b543bf274b637568354369
marcabounader92 <marcabounader92@outlook.com>
2023-07-18 19:19:17 +0300

    JS: added click function

---
+++ src/scripts/script.js
@@ -0,0 +1,45 @@
+$(document).ready(()=>{
+    let total=0;
+    let expenses_regex= new RegExp('^[0-9]*$');
+    create_expense = (name,amount) =>{
+        return `  
+        <tr>
+            <td>${name}</td>
+            <td id="amount">${amount}</td>
+            <td><button class="btn-remove">remove</button></td>
+        </tr> 
+        `;
+    }
+    $('#btn').click(()=>{
+        let expense_name=$("#expense-name");
+        let expense_amount=$("#expense-amount");
+        let name_error=$("#name-error");
+        let amount_error=$("#amount-error");
+        if(expense_name.val()){
+            if(expense_amount.val()){
+                if(expenses_regex.test(parseInt(expense_amount.val()))){
+                    let expenses_table=$("#expenses-table");
+                    let total_expenses=$("#total-expenses")
+                    let new_expense=$(create_expense(expense_name.val(),expense_amount.val()));
+                    total+=parseInt(expense_amount.val());
+                    expenses_table.append(new_expense);
+                    total_expenses.text(`Total Expenses = ${total}`);
+                    new_expense.find(".btn-remove").click(()=>{
+                        new_expense.remove();
+                        total-=parseInt(new_expense.find("#amount").text());
+                        total_expenses.text(`Total Expenses = ${total}`);
+                    });
+
+                } else{
+                    amount_error.text('Expense amount must be a number!')
+                }
+
+            }else{
+                amount_error.text('Expense amount empty!')
+            }
+        } else{
+            name_error.text('Expense name empty!')
+        }
+
+    })
+})
